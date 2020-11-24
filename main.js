b3dd14505fde6e7079b081553e2589eb6e16a0f9
Alba De biase <debiasealba@gmail.com>
2020-11-24 15:55:37 +0100

    copio mia api

---
+++ main.js
@@ -0,0 +1,16 @@
+// Milestone 1
+
+var app=new Vue ({
+    el:"#container",
+    data: {
+
+    },
+    mounted(){
+        axios
+        .get("https://api.themoviedb.org/3/movie/550?api_key=610e99fa99f9f0a61db03d2556de6388")
+            params{
+
+            }
+        .then()
+    })
+})
